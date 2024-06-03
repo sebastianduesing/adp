ea8086b1fbfb7da881725526e8116082c78619b5
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2024-06-03 14:15:30 -0500

    First pass at collecting ascii data

--- scripts/collect_normalization_data.py
+++ scripts/collect_normalization_data.py
@@ -2,6 +2,25 @@ from converter import TSV2dict
 import csv
 import sys
 
+
+def find_changed_chars(string1, string2):
+    """
+    Identifies characters in a string that were replaced with other characters
+    during ascii normalization. Returns two strings.
+    """
+    charlist1 = []
+    charlist2 = []
+    for i in string1:
+        if i not in string2:
+            charlist1.append(i)
+    for i in string2:
+        if i not in string1:
+            charlist2.append(i)
+    charlist1 = ", ".join(charlist1)
+    charlist2 = ", ".join(charlist2)
+    return charlist1, charlist2
+
+
 def collect_data(tracker_dict):
     """
     Creates a dict of Y/N counts from the character normalization data tracker sheet.
@@ -10,8 +29,17 @@ def collect_data(tracker_dict):
     -- return: A dict of Y/N counts by field.
     """
     datadict = {}
+    ascii_dict = {}
     ignored_fieldnames = ["before_char_normalization", "after_char_normalization", "index"]
     for index, rowdict in tracker_dict.items():
+        if rowdict["convert_to_ascii"] == "Y":
+            ascii_dict[index] = {}
+            ascii_dict[index]["index"] = rowdict["index"]
+            ascii_dict[index]["before_char_normalization"] = rowdict["before_char_normalization"]
+            ascii_dict[index]["after_char_normalization"] = rowdict["after_char_normalization"]
+            non_ascii, ascii = find_changed_chars(rowdict["before_char_normalization"], rowdict["after_char_normalization"])
+            ascii_dict[index]["non_ascii_characters"] = non_ascii
+            ascii_dict[index]["ascii_character_replacements"] = ascii
         for fieldname, fieldvalue in rowdict.items():
             if fieldname not in ignored_fieldnames:
                 if fieldname not in datadict.keys():
@@ -24,13 +52,14 @@ def collect_data(tracker_dict):
                     N_count = datadict[fieldname]["N"]
                     N_count += 1
                     datadict[fieldname]["N"] = N_count
-    return datadict
+    return datadict, ascii_dict
 
 
 if __name__ == "__main__":
-    char_norm_data_dict = TSV2dict(sys.argv[1])
-    data_path = sys.argv[2]
-    data = collect_data(char_norm_data_dict)
+    char_norm_data_dict = TSV2dict(sys.argv[1])  # path to character normalization data TSV
+    data_path = sys.argv[2]  # path to where collected data will be stored
+    ascii_path = sys.argv[3]  # path to where ascii tracker data will be stored
+    data, ascii_data = collect_data(char_norm_data_dict)
     with open(data_path, "w", newline = "") as tsv:
         fieldnames = ["normalization_stage", "strings_altered", "strings_unaltered"]
         writer = csv.DictWriter(tsv, fieldnames=fieldnames, delimiter="\t")
@@ -41,4 +70,17 @@ if __name__ == "__main__":
                                 "strings_altered": counts["Y"],
                                 "strings_unaltered": counts["N"]
                             })
+    with open(ascii_path, "w", newline = "") as tsv:
+        fieldnames = [
+            "index",
+            "before_char_normalization",
+            "after_char_normalization",
+            "non_ascii_characters",
+            "ascii_character_replacements"
+        ]
+        writer = csv.DictWriter(tsv, fieldnames=fieldnames, delimiter="\t")
+        writer.writeheader()
+        for index, row in ascii_data.items():
+            writer.writerow(row)
+    print(f"{ascii_path} written and saved.")
     print(f"{data_path} written and saved.")
