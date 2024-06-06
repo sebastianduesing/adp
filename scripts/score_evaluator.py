3360a5154e6cdd632a22e2f0b8c60ea6db66c7a2
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2024-06-06 14:39:14 -0500

    Update score evaluator to include overall score & add rounding

--- scripts/score_evaluator.py
+++ scripts/score_evaluator.py
@@ -8,14 +8,17 @@ def gather_score_data(normalized_data, original_column, output_path):
     c_norm_score_col = f"char_normalization_score_{original_column}"
     w_norm_score_col = f"word_normalization_score_{original_column}"
     p_norm_score_col = f"phrase_normalization_score_{original_column}"
+    overall_score_col = f"overall_normalization_score_{original_column}"
     c_score_list = []
     w_score_list = []
     p_score_list = []
+    o_score_list = []
     for index, rowdict in normalized_data.items():
         pairs = [
             (c_score_list, rowdict[c_norm_score_col]),
             (w_score_list, rowdict[w_norm_score_col]),
-            (p_score_list, rowdict[p_norm_score_col])
+            (p_score_list, rowdict[p_norm_score_col]),
+            (o_score_list, rowdict[overall_score_col])
         ]
         for (scorelist, value) in pairs:
             try:
@@ -23,7 +26,7 @@ def gather_score_data(normalized_data, original_column, output_path):
             except TypeError:
                 pass
     score_data = {}
-    for scores in [c_score_list, w_score_list, p_score_list]:
+    for scores in [c_score_list, w_score_list, p_score_list, o_score_list]:
         if scores == c_score_list:
             score_data["character_normalization_score"] = {}
             working_dict = score_data["character_normalization_score"]
@@ -36,12 +39,16 @@ def gather_score_data(normalized_data, original_column, output_path):
             score_data["phrase_normalization_score"] = {}
             working_dict = score_data["phrase_normalization_score"]
             working_dict["phase"] = "phrase"
+        if scores == o_score_list:
+            score_data["overall_normalization_score"] = {}
+            working_dict = score_data["overall_normalization_score"]
+            working_dict["phase"] = "overall"
         working_dict["minimum"] = min(scores)
         working_dict["maximum"] = max(scores)
         working_dict["median"] = np.median(scores)
-        working_dict["mean"] = np.mean(scores)
-        working_dict["standard_deviation"] = np.std(scores)
-        working_dict["variance"] = np.var(scores)
+        working_dict["mean"] = round(np.mean(scores), 2)
+        working_dict["standard_deviation"] = round(np.std(scores), 2)
+        working_dict["variance"] = round(np.var(scores), 2)
     with open(output_path, "w", newline="") as tsvfile:
         fieldnames = [
             "phase",
