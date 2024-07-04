bed8190002599327f8357792c3ba3e0b774cb39c
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2024-07-04 11:43:18 -0500

    Add toolkit and update files with minor changes

--- new/scripts/char_normalizer.py
+++ new/scripts/char_normalizer.py
@@ -1,15 +1,16 @@
 import os
 import re
 import sys
+import toolkit as tk
 from converter import TSV2dict, dict2TSV
 
 
-approved_chars = r"[a-zA-Z\d ,.+/<>()-]"
+approved_chars = r"[a-zA-Z\d ,.+/<>()-:]"
 
 
 def identify_invalid_chars(string):
     """
-    Returns a set of invalid characters found in input string.
+    Return a set of invalid characters found in input string.
     """
     invalid_chars = set()
     for char in string:
@@ -19,93 +20,14 @@ def identify_invalid_chars(string):
     return invalid_chars
 
 
-def validate(invalid_char_set, mode):
-    """
-    Returns an indicator about whether a string contains only valid chars.
-
-    Validation passes if there is nothing in invalid_char_set.
-    Passing indicator = True if mode is "boolean", "pass" otherwise.
-    Failing indicator = False if mode is "boolean", "fail" otherwise.
-    """
-    if len(invalid_char_set) == 0:
-        if mode == "boolean":
-            return True
-        else:
-            return "pass"
-    else:
-        if mode == "boolean":
-            return False
-        else:
-            return "fail"
-
-
-def check_action(rowdict):
-    """
-    Checks whether the user has specified an action to be taken in review file.
-
-    Examines one row in the review file.
-    Returns action name if the user has done exactly one of the following:
-    indicated a character to replace the target character with, marked that
-    character for removal, marked that character as invalidating the string, or
-    designated that character as allowable.
-    Returns False otherwise.
-    """
-    actions = [
-        rowdict["replace_with"] != "",
-        rowdict["remove"] != "",
-        rowdict["invalidate"] != "",
-        rowdict["allow"] != ""
-    ]
-    truth_value = 0
-    for action in actions:
-        if action:
-            truth_value += 1
-    if truth_value == 1:
-        for action in ["replace_with", "remove", "invalidate", "allow"]:
-            if rowdict[action] != "":
-                return action
-    else:
-        return None
-
-
-def update_reference(review_dict, reference_dict):
-    """
-    Moves review_dict rows in which an action has been taken to reference_dict.
-    """
-    if len(review_dict.keys()) != 0:
-        indices_to_transfer = []
-        for index, rowdict in review_dict.items():
-            if check_action(rowdict) is not None:
-                indices_to_transfer.append(index)
-        for index in indices_to_transfer:
-            new_index = highest_index(reference_dict) + 1
-            row = review_dict[index].copy()
-            row["index"] = new_index
-            reference_dict[new_index] = row
-            del review_dict[index]
-    return review_dict, reference_dict
-
-
-def lookup(invalid_char, review_dict, reference_dict):
-    """
-    Finds index of a character if it is logged in either reference or review.
-    """
-    source, location = None, None
-    for index, rowdict in reference_dict.items():
-        if invalid_char == rowdict["invalid_character"]:
-            source, location = "reference", index
-            break
-    if source is None:
-        for index, rowdict in review_dict.items():
-            if invalid_char == rowdict["invalid_character"]:
-                source, location = "review", index
-                break
-    return source, location
-
-
 def take_action(reference_dict, invalid_char, data_item, index):
     """
     Alters data_item as specified in reference_dict.
+
+    This may be moved to toolkit later; I have suspicions that word
+    substitution will require more finesse (dealing with punctuation and
+    delimiters, etc.), so it will probably be preferable to just build out a
+    word-specific version of this function. To be determined.
     """
     if reference_dict[index]["replace_with"] != "":
         replacement = reference_dict[index]["replace_with"]
@@ -141,17 +63,6 @@ def basic_normalize(data_item, index):
     return data_item, item_change_dict
 
 
-def highest_index(dict_with_index):
-    """
-    Finds highest index in a dict and returns it.
-    """
-    if len(dict_with_index.keys()) == 0:
-        return -1
-    else:
-        indices = [int(i) for i in dict_with_index.keys()]
-        return max(indices)
-
-
 def normalize_chars(style, data_file, target_column, review_file, reference_file):
     """
     Performs character normalization on target_column in data_file.
@@ -172,18 +83,18 @@ def normalize_chars(style, data_file, target_column, review_file, reference_file
         reference_dict = TSV2dict(reference_file)
     else:
         reference_dict = {}
-    review_dict, reference_dict = update_reference(review_dict, reference_dict)
+    review_dict, reference_dict = tk.update_reference(review_dict, reference_dict)
     for index, rowdict in data_dict.items():
         data_item = rowdict[target_column]
         data_item, changes = basic_normalize(data_item, index)
         char_change_dict[index] = changes
         invalid_chars = identify_invalid_chars(data_item)
-        rowdict["char_validation"] = validate(invalid_chars, "string")
-        if validate(invalid_chars, "boolean"):
+        rowdict["char_validation"] = tk.validate(invalid_chars, "string")
+        if tk.validate(invalid_chars, "boolean"):
             rowdict[f"char_normalized_{target_column}"] = data_item
         else:
             for char in invalid_chars:
-                source, location = lookup(char, review_dict, reference_dict)
+                source, location = tk.lookup(char, review_dict, reference_dict, "char")
                 if source == "reference":
                     data_item = take_action(reference_dict,
                                             char,
@@ -196,13 +107,17 @@ def normalize_chars(style, data_file, target_column, review_file, reference_file
                         if len(review_dict[location]["context"]) < 300:
                             context_string = review_dict[location]["context"]
                             context_string += f""", '{data_item}'"""
+                            occurrences = review_dict[location]["occurrences"]
+                            occurrences += 1
+                            review_dict[location]["occurrences"] = occurrences
                             review_dict[location]["context"] = context_string
                 else:
-                    id = highest_index(review_dict) + 1
+                    id = tk.highest_index(review_dict) + 1
                     review_dict[id] = {}
                     review_dict[id]["index"] = id
                     review_dict[id]["invalid_character"] = char
                     review_dict[id]["context"] = f"""'{data_item}'"""
+                    review_dict[id]["occurrences"] = 1
                     review_dict[id]["replace_with"] = ""
                     review_dict[id]["remove"] = ""
                     review_dict[id]["invalidate"] = ""
@@ -211,8 +126,8 @@ def normalize_chars(style, data_file, target_column, review_file, reference_file
             for char in invalid_chars.copy():
                 if char in allowed_chars:
                     invalid_chars.remove(char)
-            rowdict["char_validation"] = validate(invalid_chars, "string")
-            if validate(invalid_chars, "boolean"):
+            rowdict["char_validation"] = tk.validate(invalid_chars, "string")
+            if tk.validate(invalid_chars, "boolean"):
                 rowdict[f"char_normalized_{target_column}"] = data_item
             else:
                 rowdict[f"char_normalized_{target_column}"] = "!"
---
+++ new/scripts/toolkit.py
@@ -0,0 +1,101 @@
+import re
+
+
+def check_action(rowdict):
+    """
+    Check whether the user has specified an action to be taken in review file.
+
+    Examines one row in the review file.
+    Returns action name if the user has done exactly one of the following:
+    indicated a character to replace the target character with, marked that
+    character for removal, marked that character as invalidating the string, or
+    designated that character as allowable.
+    Returns False otherwise.
+    """
+    actions = [
+        rowdict["replace_with"] != "",
+        rowdict["remove"] != "",
+        rowdict["invalidate"] != "",
+        rowdict["allow"] != ""
+    ]
+    truth_value = 0
+    for action in actions:
+        if action:
+            truth_value += 1
+    if truth_value == 1:
+        for action in ["replace_with", "remove", "invalidate", "allow"]:
+            if rowdict[action] != "":
+                return action
+    else:
+        return None
+
+
+def highest_index(dict_with_index):
+    """
+    Finds highest index in a dict and returns it.
+    """
+    if len(dict_with_index.keys()) == 0:
+        return -1
+    else:
+        indices = [int(i) for i in dict_with_index.keys()]
+        return max(indices)
+
+
+def lookup(invalid_item, review_dict, reference_dict, mode):
+    """
+    Finds index of a character if it is logged in either reference or review.
+    """
+    if mode == "char":
+        mode = "character"
+    source, location = None, None
+    for index, rowdict in reference_dict.items():
+        if invalid_item == rowdict[f"invalid_{mode}"]:
+            source, location = "reference", index
+            break
+    if source is None:
+        for index, rowdict in review_dict.items():
+            if invalid_item == rowdict[f"invalid_{mode}"]:
+                source, location = "review", index
+                break
+    return source, location
+
+
+def update_reference(review_dict, reference_dict):
+    """
+    Moves review_dict rows in which an action has been taken to reference_dict.
+    """
+    if len(review_dict.keys()) != 0:
+        indices_to_transfer = []
+        for index, rowdict in review_dict.items():
+            if check_action(rowdict) is not None:
+                indices_to_transfer.append(index)
+        for index in indices_to_transfer:
+            new_index = highest_index(reference_dict) + 1
+            row = review_dict[index].copy()
+            row["index"] = new_index
+            reference_dict[new_index] = row
+            del review_dict[index]
+    return review_dict, reference_dict
+
+
+def validate(invalid_set, mode):
+    """
+    Return an indicator about whether a string contains only valid items.
+
+    Validation passes if there is nothing in invalid_set.
+    Passing indicator = True if mode is "boolean", "pass" otherwise.
+    Failing indicator = False if mode is "boolean", "fail" otherwise.
+    """
+    if len(invalid_set) == 0:
+        if mode == "boolean":
+            return True
+        else:
+            return "pass"
+    else:
+        if mode == "boolean":
+            return False
+        else:
+            return "fail"
+
+
+
