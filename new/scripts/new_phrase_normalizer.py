3177151f4ebdc3e98a35bd704c59f913ce9af530
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2024-07-15 16:51:22 -0500

    Add phrase-reorganization functionality

--- new/scripts/new_phrase_normalizer.py
+++ new/scripts/new_phrase_normalizer.py
@@ -4,7 +4,7 @@ import sys
 import toolkit as tk
 from converter import TSV2dict, dict2TSV
 
-separator = r"[-,\.():;\s]+"
+separator = r"[-,\():;\s]+"
 
 
 def find_category(word, word_review_dict, word_reference_dict):
@@ -60,8 +60,44 @@ def make_categorization_string(phrase_dict):
     cat_string = ""
     for index, word_dict in phrase_dict.items():
         category = word_dict["category"]
-        cat_string += f"[{category}]"
+        cat_string += f"[{category}({index})]"
     return cat_string
+
+
+def phrase_lookup(cat_string):
+    matched = False
+    for index, rowdict in type_dict.items():
+        pattern = rowdict["pattern"]
+        if cat_string == pattern:
+            return rowdict["name"], rowdict["valid?"], rowdict["standard_form"]
+            matched = True
+            break
+        elif "|" in pattern:
+            pattern = pattern.split("|")
+            standard_form = rowdict["standard_form"]
+            if standard_form != "":
+                standard_form = standard_form.split()
+            for i in range(len(pattern)):
+                if cat_string == pattern[i]:
+                    if standard_form != "":
+                        standard_form = standard_form[i]
+                    matched = True
+                    return rowdict["name"], rowdict["valid?"], standard_form
+                    break
+    if not matched:
+        return "unknown", "N", ""
+
+
+def rearrange_phrase(cat_string, rowdict, phrase_dict):
+    p_type, validity, standard_form = phrase_lookup(cat_string)
+    rowdict["phrase_type"] = p_type
+    rowdict["phrase_validation"] = "pass" if validity == "Y" else "fail"
+    phrase = standard_form
+    match_list = re.findall(r"\d+", standard_form)
+    for match in match_list:
+        referent = phrase_dict[int(match)]["word"]
+        phrase = re.sub(fr"\[{match}\]", referent, phrase)
+    rowdict["phrase_normalized"] = phrase
 
 
 def normalize_phrase(style, data_file, original_column):
@@ -69,13 +105,19 @@ def normalize_phrase(style, data_file, original_column):
     data_dict = TSV2dict(data_file)
     for index, rowdict in data_dict.items():
         data_item = rowdict[target_column]
-        stopped = re.fullmatch(r"! .* !", data_item)
-        if stopped:
-            rowdict[f"phrase_type_string"] = "[invalid]"
-            continue
-        phrase_dict = build_phrase_dict(data_item, separator)
-        cat_string = make_categorization_string(phrase_dict)
-        rowdict[f"phrase_type_string"] = cat_string
+        if rowdict["word_validation"] == "fail" or rowdict["word_validation"] == "stopped":
+            rowdict["phrase_type_string"] = "stopped"
+            rowdict["phrase_type"] = "stopped"
+            rowdict["phrase_validation"] = "stopped"
+            rowdict["phrase_normalized"] = data_item
+        else:
+            phrase_dict = build_phrase_dict(data_item, separator)
+            cat_string = make_categorization_string(phrase_dict)
+            rowdict["phrase_type_string"] = cat_string
+            rearrange_phrase(cat_string, rowdict, phrase_dict)
+            if rowdict["phrase_validation"] == "fail":
+                phrase_type = rowdict["phrase_type"]
+                rowdict["phrase_normalized"] = f"! Invalid phrase type: {phrase_type} !"
     output_path = os.path.join(style, "output_files", f"p_norm_{style}.tsv")
     dict2TSV(data_dict, output_path)
 
@@ -86,6 +128,8 @@ if __name__ == "__main__":
     input_file = os.path.join(style, "output_files", f"w_norm_{style}.tsv")
     word_review_file = os.path.join(style, "output_files", "word_review.tsv")
     word_reference_file = os.path.join(style, "output_files", "word_reference.tsv")
+    type_file = os.path.join(style, "input_files", f"{style}_phrase_types.tsv")
     word_review_dict = TSV2dict(word_review_file)
     word_reference_dict = TSV2dict(word_reference_file)
+    type_dict = TSV2dict(type_file)
     normalize_phrase(style, input_file, original_column)
